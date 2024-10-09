67f9d5815a50e11882f598762a7993727e7a59cd
Xiaojing.Chen <d29107d@gmail.com>
2024-10-09 09:33:02 +0800

    autoindent

--- lua/options.lua
+++ lua/options.lua
@@ -3,3 +3,7 @@ vim.wo.relativenumber = false
 vim.opt.mouse = ""
 -- vim.g.encoding = "utf-8"
 vim.o.fileencodings = "ucs-bom,utf-8,gb2312,cp936,gb18030,big5,euc-jp,euc-kr,latin1"
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.autoindent = true
