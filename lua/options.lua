421c34ea0f5f4d26b67dd705a6d38055deaa6578
Xiaojing.Chen <d29107d@gmail.com>
2024-10-25 19:53:56 +0800

    relativenumber true

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,5 @@
 vim.wo.number = true
-vim.wo.relativenumber = false
+-- vim.wo.relativenumber = false
 vim.opt.mouse = ""
 -- vim.g.encoding = "utf-8"
 vim.o.fileencodings = "ucs-bom,utf-8,gb2312,cp936,gb18030,big5,euc-jp,euc-kr,latin1"
